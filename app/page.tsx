387733f211dc3a46f6c191e82b4bcc9398b36735
DinhKhoi22 <111755052+DinhKhoi22@users.noreply.github.com>
2023-11-20 16:33:32 +0700

    delete the link to drive

--- app/page.tsx
+++ app/page.tsx
@@ -10,14 +10,6 @@ export default async function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="absolute">
-        <div className="text-white mb-10">
-          Ảnh chụp database{' '}
-          <Link href={'https://drive.google.com/drive/folders/1U-bNpUqJJtfJT5QW2_UuD0KRIrnorPYy?usp=sharing'} target='_blank'>
-            <button className="bg-white hover:bg-gray-300 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow">
-              ở đây
-            </button>
-          </Link>
-        </div>
         {blogs.map((blog: any) => (
           <SingleBlog key={blog._id} data={blog} currentUser={currentUser} />
         ))}
